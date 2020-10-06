82a215d5d22a9ac3204bbf92d347d0b99465d683
Timmeahj <broostim@hotmail.be>
2020-10-06 15:57:06 +0200

    check for combination added

--- script.js
+++ script.js
@@ -9,14 +9,41 @@ var totalResults = [];
 
 document.getElementById('run').addEventListener('click', function () {
     let amount = document.getElementById('amount').value;
+    if(amount > 1000000){
+        amount = 1000000;
+    }
     for(let j = 0; j < amount; j++){
         let draw = lotteryDraw();
-        for(let i =0; i < draw.length; i++){
+        /*for(let i =0; i < draw.length; i++){
             totalResults.push(draw[i]);
         }
+         */
+        totalResults.push(draw);
     }
     printResults(totalResults);
 });
+
+document.getElementById('findCombo').addEventListener('click', function () {
+    let combo = document.getElementById('combo').value.split(" ");
+    combo = fixCombo(combo);
+    checkForCombination(combo);
+});
+
+document.getElementById('goUntilCombo').addEventListener('click', function () {
+    let combo = document.getElementById('combo').value.split(" ");
+    combo = fixCombo(combo);
+    do {
+        totalResults.push(lotteryDraw());
+    } while (checkUntilFound(combo));
+});
+
+function fixCombo(combo) {
+    let parsedCombo = [];
+    for(let i = 0; i < combo.length; i++){
+        parsedCombo.push(parseInt(combo[i]));
+    }
+    return parsedCombo;
+}
 
 function lotteryDraw() {
     let numbers = [];
@@ -47,8 +74,49 @@ function printResults(results) {
     }
 }
 
-function getOccurrence(array, value) {
+function getOccurrence(arr, value) {
     var count = 0;
-    array.forEach((v) => (v === value && count++));
+    for(let i = 0; i < arr.length; i++){
+        arr[i].forEach((v) => (v === value && count++));
+    }
     return count;
+}
+
+function checkForCombination(combo){
+    let found = false;
+    for (let i = 0; i < totalResults.length; i++){
+        if(arraysEqual(totalResults[i], combo)){
+            document.getElementById('feedback').innerHTML = "combination found after "+i+" iterations";
+            found = true;
+        }
+    }
+    if(!found){
+        console.log(combo);
+        document.getElementById('feedback').innerHTML = "combination not found";
+    }
+}
+
+function checkUntilFound(combo) {
+    let notFound = true;
+    for (let i = 0; i < totalResults.length; i++){
+        if(arraysEqual(totalResults[i], combo)){
+            console.log("combination found on position "+i);
+            notFound = false;
+        }
+    }
+    return notFound;
+}
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    a.sort();
+    b.sort();
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
 }
